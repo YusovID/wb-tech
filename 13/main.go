471942da4e7f033827d2f642b0e0b5f99d4f1e62
Ilya Yusov <yusovilya@gmail.com>
2025-10-26 23:14:54 +0300

    выполнил задание 13

--- 13/main.go
+++ 13/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	num1, num2 := 0, 1
+	fmt.Printf("num1 = %d, num2 = %d\n", num1, num2)
+
+	switchByAddAndSub(&num1, &num2)
+	fmt.Printf("num1 = %d, num2 = %d\n", num1, num2)
+
+	switchByXOR(&num1, &num2)
+	fmt.Printf("num1 = %d, num2 = %d\n", num1, num2)
+}
+
+func switchByAddAndSub(num1, num2 *int) {
+	*num1 = *num1 - *num2
+	*num2 = *num2 + *num1
+	*num1 = *num2 - *num1
+}
+
+func switchByXOR(num1, num2 *int) {
+	*num1 ^= *num2
+	*num2 ^= *num1
+	*num1 ^= *num2
 }
