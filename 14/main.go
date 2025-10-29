97ca7d34aa435853bbdae06d9d272a442578ca07
Ilya Yusov <yusovilya@gmail.com>
2025-10-29 18:21:17 +0300

    выполнил задание 14

--- 14/main.go
+++ 14/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var1 := 1
+	var2 := "string"
+	var3 := true
+	var4 := make(chan interface{})
+	var5 := []int{}
+
+	fmt.Printf("type of var1: %s\n", typeOf(var1))
+	fmt.Printf("type of var2: %s\n", typeOf(var2))
+	fmt.Printf("type of var3: %s\n", typeOf(var3))
+	fmt.Printf("type of var4: %s\n", typeOf(var4))
+	fmt.Printf("type of var5: %s\n", typeOf(var5))
+}
+
+func typeOf(v interface{}) string {
+	switch v.(type) {
+	case int:
+		return "int"
+	case string:
+		return "string"
+	case bool:
+		return "bool"
+	case chan interface{}:
+		return "chan"
+	default:
+		return "unknown type"
+	}
 }
