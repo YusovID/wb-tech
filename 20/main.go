3d560142a781222294b403988e130dfbed4c8e62
Ilya Yusov <yusovilya@gmail.com>
2025-11-08 19:51:09 +0300

    выполнил задание 20

--- 20/main.go
+++ 20/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
 
+func main() {
+	fmt.Println(flipOverWords("snow dog sun"))
+}
+
+func flipOverWords(str string) string {
+	words := strings.Split(str, " ")
+	slices.Reverse(words)
+	return strings.Join(words, " ")
 }
