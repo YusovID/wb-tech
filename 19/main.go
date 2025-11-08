752b420d3794c0bcd358be4182387a9724181d5c
Ilya Yusov <yusovilya@gmail.com>
2025-11-08 19:32:37 +0300

    выполнил задание 19

--- 19/main.go
+++ 19/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"slices"
+)
 
+func main() {
+	fmt.Println(flipOverString(`Разработать программу, которая переворачивает подаваемую на вход строку.
+
+Например: при вводе строки «главрыба» вывод должен быть «абырвалг».
+
+Учтите, что символы могут быть в Unicode (русские буквы, emoji и пр.), то есть просто iterating по байтам может не подойти — нужен срез рун ([]rune).`))
+}
+
+func flipOverString(str string) string {
+	runes := []rune(str)
+	slices.Reverse(runes)
+	return string(runes)
 }
