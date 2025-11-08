f068b424697277ece731b1bfe7468e467fdd65e8
Ilya Yusov <yusovilya@gmail.com>
2025-11-09 02:17:14 +0300

    выполнил задание 25

--- 25/main.go
+++ 25/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+var timeToSleep = 1 // секунд
+
+func main() {
+	for i := range 10 {
+		sleep(timeToSleep)
+		fmt.Printf("i: %d, current time: %v\n", i, time.Now().Format("2006-01-02 15:04:05"))
+	}
+}
+
+func sleep(duration int) {
+	ticker := time.NewTicker(time.Duration(duration) * time.Second)
+	defer ticker.Stop()
+
+	// блокируемся, пока в тикере не произойдет "тик"
+	<-ticker.C
 }
