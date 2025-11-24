1092329e6ad968afb0c3487e42c58cd5da169c78
Ilya Yusov <yusovilya@gmail.com>
2025-11-24 22:37:55 +0300

    выполнил задание 5

--- 5/main.go
+++ 5/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+// структура customError соответствует интерфейсу error, т.к. имеет метод Error()
+type customError struct {
+	msg string
+}
 
+func (e *customError) Error() string {
+	return e.msg
+}
+
+// в функции test мы возвращаем указатель на структуру customError, значение которой равно nil
+func test() *customError {
+	// ... do something
+	return nil
+}
+
+func main() {
+	var err error
+	err = test()
+	// при проверке, равен ли интерфейс err nil, условие выполнится,
+	// т.к. интерфейс содержит динамический тип customError, а не nil.
+	// Чтобы условие выполнилось, надо чтобы и динамический тип интерфейса, и его значение были nil,
+	// поэтому выведется error
+	if err != nil {
+		println("error")
+		return
+	}
+	println("ok")
 }
